30e6b7a8ef59a900f5570dc1db521a931b459317
r <64125318+spill@users.noreply.github.com>
2025-04-23 21:49:31 -0700

    Optimization of smoothness of snake + include Game over object

--- Assets/Scripts/Snakes.cs
+++ Assets/Scripts/Snakes.cs
@@ -1,104 +1,71 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-// Todo make a parent class "snake" that has snake1 and snake2
 public class Snakes : MonoBehaviour
 {
-    // Snake intially moves up
     private Vector2 direction = Vector2.up;
-    // List to keep track of body parts
     private List<Transform> body;
-    // Used to check if game is active 
     private bool IsGameOver = false;
-    // Parts used to make the snake
     public Transform bodyPrefab;
-
     public float SnakeSpeed = 16.0f;
 
-    // controls
-    
-
-    // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1f;
         body = new List<Transform>();
         body.Add(this.transform);
     }
-    // Extends body by one part/square
+
     private void Grow()
     {
         Transform block = Instantiate(this.bodyPrefab);
         block.position = body[body.Count - 1].position;
-
         body.Add(block);
     }
-
-    // private void Reset()
-    // {
-        // Clear the snake
-        // Move snake to intial position
-        
-    // }
 
     private void GameOver()
     {
         SnakeSpeed = 0.0f;
-        // disable movement
-        // play sound effect 
+        IsGameOver = true;
+        FindObjectOfType<GameOver>().TriggerGameOver();
     }
-    // Function to handle collisions
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // When snake touches apple increase size
-        if (other.tag == "Apple") {
+        if (other.tag == "Apple")
+        {
             Grow();
         }
-        // If snake touches wall or itself, end the game
         else if (other.tag == "Collidable")
         {
-            IsGameOver = true;
             GameOver();
-            // Reset();
         }
     }
-    // Update is called once per frame
+
     private void Update()
     {
-        // Handles direction inputs (using WASD)
-       if (Input.GetKeyDown(KeyCode.UpArrow) && direction != Vector2.down)
-       {
-        direction = Vector2.up;
-       } 
-       else if (Input.GetKeyDown(KeyCode.DownArrow) && direction != Vector2.up)
-       {
-        direction = Vector2.down;
-       }
-       else if (Input.GetKeyDown(KeyCode.LeftArrow) && direction != Vector2.right)
-       {
-        direction = Vector2.left;
-       }
-       else if (Input.GetKeyDown(KeyCode.RightArrow) && direction != Vector2.left)
-       {
-        direction = Vector2.right;
-       }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && direction != Vector2.down)
+            direction = Vector2.up;
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && direction != Vector2.up)
+            direction = Vector2.down;
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) && direction != Vector2.right)
+            direction = Vector2.left;
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && direction != Vector2.left)
+            direction = Vector2.right;
     }
+
     private void FixedUpdate()
     {
         if (!IsGameOver)
         {
-        for (int i = body.Count - 1; i > 0; i--)
-        {
-            body[i].position = body[i - 1].position;
-        }
-        this.transform.position = new Vector3(
-            Mathf.Round(this.transform.position.x + direction.x),
-            Mathf.Round(this.transform.position.y + direction.y),
-            0.0f
-        );
+            for (int i = body.Count - 1; i > 0; i--)
+                body[i].position = body[i - 1].position;
+
+            this.transform.position = new Vector3(
+                Mathf.Round(this.transform.position.x + direction.x),
+                Mathf.Round(this.transform.position.y + direction.y),
+                0.0f
+            );
         }
     }
 }
-
